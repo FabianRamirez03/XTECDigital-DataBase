6312063938d54685083b4d31964901eb55c5fa59
Fabian <ramirezfabian003@gmail.com>
2020-12-03 00:12:32 -0600

    Agregado el login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {HttpClient, HttpClientModule, HttpResponse} from '@angular/common/http';
+
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -15,7 +17,8 @@ import { ListacursosComponent } from './admin/listacursos/listacursos.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient, HttpResponse} from '@angular/common/http';
+import {Router} from '@angular/router';
+import {Obj} from '@popperjs/core';
 
 @Component({
   selector: 'app-login',
@@ -8,10 +11,24 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   user: any;
   password: any;
-  constructor() { }
+  existe: any;
+  constructor(public httpService: HttpClient, private router: Router) { }
   login(): void{
     this.user = (document.getElementById('user') as HTMLInputElement).value;
+    const userAsInteger: number = +this.user;
     this.password = (document.getElementById('password') as HTMLInputElement).value;
+    this.httpService.post('https://localhost:5001/Usuario/validarUser',
+      { carnet: userAsInteger, password: this.password}).subscribe(
+      (resp: HttpResponse<object>) => { this.existe = resp;
+
+                                        // @ts-ignore
+                                        if (resp.carnet != null){
+          console.log(resp);
+          this.router.navigate(['/', 'ListaCursos']);
+        }
+        else{
+          console.log('notwins');
+        }});
   }
   ngOnInit(): void {
   }
