23d64957ccaf39b0db6ff1b386ab9f41d925b47f
Mario Araya <marioaxtrem@gmail.com>
2020-12-07 00:49:17 -0600

    nada

--- src/app/componentes/files/files.component.ts
+++ src/app/componentes/files/files.component.ts
@@ -6,20 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./files.component.scss']
 })
 export class FilesComponent implements OnInit {
-  public hostUrl = 'https://ej2-aspcore-service.azurewebsites.net/';
+  public hostUrl = 'https://localhost:5001/Documentos';
   public ajaxSettings: object;
   public enablePersistence: boolean;
-  constructor() { }
+  public Path = '/CE3101';
+  public enableRtl: boolean;
+  constructor() {alert(this.Path); }
 
   public ngOnInit(): void {
     this.ajaxSettings = {
-      url: this.hostUrl + 'api/FileManager/FileOperations',
+      url: this.hostUrl + '/FileOperations',
       getImageUrl: this.hostUrl + 'api/FileManager/GetImage',
       uploadUrl: this.hostUrl + 'api/FileManager/Upload',
       downloadUrl: this.hostUrl + 'api/FileManager/Download'
     };
+    this.Path = '/CE3101';
+    this.enableRtl = true;
     this.enablePersistence = true;
   }
+
 
   // File Manager's file onSuccess function
   onAjaxSuccess(args: any): any {
