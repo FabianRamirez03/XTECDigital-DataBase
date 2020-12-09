6c7525c038d91dfce7bd9f08dc56c0b93aa615ac
Mariana Vargas Ramirez <marianavargas11209@gmail.com>
2020-12-08 18:37:22 -0600

    [add] creacion pop, arreglar pop

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ListacursosComponent } from './admin/listacursos/listacursos.component';
+import { SemestreComponent } from './admin/semestre/semestre.component';
+import { AddsemestreComponent } from './admin/addsemestre/addsemestre.component';
+import { AddProfesorComponent } from './admin/add-profesor/add-profesor.component';
+import { AddEstudianteComponent } from './admin/add-estudiante/add-estudiante.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/Login', pathMatch: 'full' },
   {path: 'Login', component: LoginComponent},
   {path: 'ListaCursos', component: ListacursosComponent},
+  {path: 'Semestre', component: SemestreComponent},
+  {path: 'AddSemestre', component: AddsemestreComponent},
+  {path: 'AddProfesor', component: AddProfesorComponent},
+  {path: 'AddEstudiante', component: AddEstudianteComponent},
 ];
 
 @NgModule({
@@ -14,4 +22,5 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [LoginComponent, ListacursosComponent];
+export const routingComponents = [LoginComponent, ListacursosComponent, SemestreComponent, AddsemestreComponent, AddProfesorComponent,
+  AddEstudianteComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatDialogModule} from '@angular/material/dialog';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 
 import {HttpClient, HttpClientModule, HttpResponse} from '@angular/common/http';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
+import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { ListacursosComponent } from './admin/listacursos/listacursos.component';
+import { SemestreComponent } from './admin/semestre/semestre.component';
+import { AddsemestreComponent } from './admin/addsemestre/addsemestre.component';
+import { AddProfesorComponent } from './admin/add-profesor/add-profesor.component';
+import { AddEstudianteComponent } from './admin/add-estudiante/add-estudiante.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {DatePipe} from '@angular/common';
+import {MessengerService} from './MessengerService';
+import {MatInputModule} from '@angular/material/input';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     routingComponents,
     LoginComponent,
-    ListacursosComponent
+    ListacursosComponent,
+    SemestreComponent,
+    AddsemestreComponent,
+    AddProfesorComponent,
+    AddEstudianteComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    MatButtonModule,
+    MatDialogModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [MessengerService, DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
