e4e8570f8ba0ad55a7886564f401948782780b7f
Fabian <ramirezfabian003@gmail.com>
2020-12-16 21:24:19 -0600

    Lista pagina de semestres

--- src/app/admin/semestre/semestre.component.ts
+++ src/app/admin/semestre/semestre.component.ts
@@ -10,7 +10,11 @@ import {HttpClient, HttpResponse} from '@angular/common/http';
   styleUrls: ['./semestre.component.scss']
 })
 export class SemestreComponent implements OnInit {
+  respuesta: any;
   semestres: any;
+  ano: any;
+  periodo: any;
+  cedulaAdmin: any;
   constructor(public dialog: MatDialog, public httpService: HttpClient) {
   }
 
@@ -18,7 +22,28 @@ export class SemestreComponent implements OnInit {
     this.setSemestres();
   }
   agregar(): void{
-    console.log('agregado');
+    this.ano = (document.getElementById('ano') as HTMLInputElement).value;
+    this.periodo = (document.getElementById('periodo') as HTMLInputElement).value;
+    this.cedulaAdmin = '11111';
+    this.httpService.post('https://localhost:5001/Semestre/crearSemestre',
+      {
+        // tslint:disable-next-line:radix
+        ano: parseInt(this.ano),
+        periodo: this.periodo,
+        cedulaAdmin: this.cedulaAdmin
+      }).subscribe(
+      (resp: HttpResponse<any>) =>
+      {
+        this.respuesta = resp;
+        // tslint:disable-next-line:triple-equals
+        if (resp[0].respuesta != 'error'){
+          this.ngOnInit();
+        }else{
+          alert(resp[0].error);
+        }
+      }
+    );
+    this.ngOnInit();
   }
   eliminar(): void{
     console.log('eliminado');
